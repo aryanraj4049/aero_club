ecc9be253dc38cc8c111e6076e8a583467a78f2c
Aryan <aryanr20@iitk.ac.in>
2023-01-28 23:44:23 +0530

    Updates

---
+++ precache-manifest.1e6ebafb6f8a7a2a29a6d0c296c99fb6.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6cd708663a357f21e3b",
+    "url": "/aero_club/static/css/main.08e264c3.chunk.css"
+  },
+  {
+    "revision": "b6cd708663a357f21e3b",
+    "url": "/aero_club/static/js/main.b6cd7086.chunk.js"
+  },
+  {
+    "revision": "276fde6ab22220919785",
+    "url": "/aero_club/static/js/1.276fde6a.chunk.js"
+  },
+  {
+    "revision": "bcefffa0b4d2aebcaffc",
+    "url": "/aero_club/static/css/2.81b681e0.chunk.css"
+  },
+  {
+    "revision": "bcefffa0b4d2aebcaffc",
+    "url": "/aero_club/static/js/2.bcefffa0.chunk.js"
+  },
+  {
+    "revision": "7ca7367f88fb669c5d45",
+    "url": "/aero_club/static/js/runtime~main.7ca7367f.js"
+  },
+  {
+    "revision": "00acdc59b1f4cbcfc3077b6a352d3f7d",
+    "url": "/aero_club/static/media/team-bg.00acdc59.mp4"
+  },
+  {
+    "revision": "adcd43f211f5282a9f8461862579b0f8",
+    "url": "/aero_club/static/media/square-instagram.adcd43f2.svg"
+  },
+  {
+    "revision": "e5280a295cd64ff828b58568c552c875",
+    "url": "/aero_club/static/media/square-facebook.e5280a29.svg"
+  },
+  {
+    "revision": "f5e9f7aa19847cd0a9651c5185c70500",
+    "url": "/aero_club/static/media/iitk-logo.f5e9f7aa.png"
+  },
+  {
+    "revision": "80fe551f01cf2ce9b6d8fefaa9b03774",
+    "url": "/aero_club/static/media/snt.80fe551f.png"
+  },
+  {
+    "revision": "9a2acf03a5b8f0dca24bd64ab123247a",
+    "url": "/aero_club/static/media/Aero-logo.9a2acf03.png"
+  },
+  {
+    "revision": "1776de2990c728470db0573c14997457",
+    "url": "/aero_club/static/media/club-room.1776de29.jpg"
+  },
+  {
+    "revision": "852ba1772afd3321af7caa106f628ad5",
+    "url": "/aero_club/static/media/club-room-2.852ba177.jpg"
+  },
+  {
+    "revision": "648e2d1e13fd0ef1ff03e2e5e5f45bac",
+    "url": "/aero_club/static/media/linkedin.648e2d1e.svg"
+  },
+  {
+    "revision": "43feddce7a863a1d44170f71b2a9e911",
+    "url": "/aero_club/static/media/discord.43feddce.png"
+  },
+  {
+    "revision": "d40baab882334b9371fd929661b45bae",
+    "url": "/aero_club/static/media/youtube.d40baab8.png"
+  },
+  {
+    "revision": "15e39405489e3aad94c29f3e97ef455f",
+    "url": "/aero_club/static/media/globe.15e39405.png"
+  },
+  {
+    "revision": "b66e88f89e8b88dd6b9c347b03d71e6e",
+    "url": "/aero_club/static/media/envelope-solid.b66e88f8.svg"
+  },
+  {
+    "revision": "ba23bc7e927aca94317ff7045920b84e",
+    "url": "/aero_club/static/media/special-menu-1.ba23bc7e.jpeg"
+  },
+  {
+    "revision": "a0aa178452094fd286e0a857f7e2f75c",
+    "url": "/aero_club/static/media/special-menu-2.a0aa1784.jpg"
+  },
+  {
+    "revision": "e75129cf1d2e0b70147bcae13a661c99",
+    "url": "/aero_club/static/media/special-menu-3.e75129cf.jpg"
+  },
+  {
+    "revision": "0b99cdaf5f0388b3467ffe09b8815808",
+    "url": "/aero_club/static/media/Takneek.0b99cdaf.jpg"
+  },
+  {
+    "revision": "876d090dab9aeefb78cb14d7f960485b",
+    "url": "/aero_club/static/media/Summer-project.876d090d.png"
+  },
+  {
+    "revision": "9275f4c42d5a53e14c809ee94d9bb1e1",
+    "url": "/aero_club/static/media/balsa.9275f4c4.jpg"
+  },
+  {
+    "revision": "727c7b192bf4475d8e4a7754e3462ab7",
+    "url": "/aero_club/static/media/Aakanksha.727c7b19.jpg"
+  },
+  {
+    "revision": "700d5fcdcabb5054aaba422dc536fd74",
+    "url": "/aero_club/static/media/Abhinaba.700d5fcd.jpg"
+  },
+  {
+    "revision": "7e410b0c898b365a7cb27514cc5ba89e",
+    "url": "/aero_club/static/media/Aditya.7e410b0c.jpg"
+  },
+  {
+    "revision": "dca92cd9acf6bd6413ec49934cb7f89b",
+    "url": "/aero_club/static/media/Alka.dca92cd9.jpg"
+  },
+  {
+    "revision": "76f2f8fc423fc39b7439f38abafa99b1",
+    "url": "/aero_club/static/media/Ambuj.76f2f8fc.jpg"
+  },
+  {
+    "revision": "1e5bec73e4cc5ffe9870da84affb4010",
+    "url": "/aero_club/static/media/Arpit.1e5bec73.jpg"
+  },
+  {
+    "revision": "4bfda655fe5ba8b9da5cb3005f93a0f3",
+    "url": "/aero_club/static/media/Himanshu.4bfda655.jpg"
+  },
+  {
+    "revision": "49235390f95430e4b2a43b6d648d212a",
+    "url": "/aero_club/static/media/Mohit.49235390.jpeg"
+  },
+  {
+    "revision": "29bef991cb08c569ef097bbbdf4883d9",
+    "url": "/aero_club/static/media/Moin.29bef991.jpg"
+  },
+  {
+    "revision": "41a44e47c602f8c361e3d256d63eda68",
+    "url": "/aero_club/static/media/Netraj.41a44e47.jpg"
+  },
+  {
+    "revision": "df13c9bcbad2ad69193b79532125bf58",
+    "url": "/aero_club/static/media/Omkar.df13c9bc.jpeg"
+  },
+  {
+    "revision": "a17b39f16957cda2c00596f2fadc3199",
+    "url": "/aero_club/static/media/Piyush.a17b39f1.jpg"
+  },
+  {
+    "revision": "6da08f246f94624e17cb261a03090e5a",
+    "url": "/aero_club/static/media/Prabuddha.6da08f24.png"
+  },
+  {
+    "revision": "7b42fb1dd97f10ec103d2be145dc52b8",
+    "url": "/aero_club/static/media/Pranshu.7b42fb1d.jpg"
+  },
+  {
+    "revision": "e01d78ec3e349e7e69debdb87392c891",
+    "url": "/aero_club/static/media/Pulak.e01d78ec.png"
+  },
+  {
+    "revision": "59b0235ac18e13d9b54a22fc04664445",
+    "url": "/aero_club/static/media/Rahul.59b0235a.jpg"
+  },
+  {
+    "revision": "bc6374d3f7ae2c51d57f7546213ecbae",
+    "url": "/aero_club/static/media/Rishima.bc6374d3.jpg"
+  },
+  {
+    "revision": "01af6f0e29ef9f806d588d0a4a9d0bb2",
+    "url": "/aero_club/static/media/Ritwik.01af6f0e.jpg"
+  },
+  {
+    "revision": "3592249ef0520f442b1376e335f21444",
+    "url": "/aero_club/static/media/Sarthak.3592249e.jpg"
+  },
+  {
+    "revision": "6ce4d2ea268625802dbe3ff496b7d9ca",
+    "url": "/aero_club/static/media/Shrey.6ce4d2ea.jpg"
+  },
+  {
+    "revision": "32ced264cae063eb53187cb271b25f76",
+    "url": "/aero_club/static/media/Shreyash.32ced264.jpg"
+  },
+  {
+    "revision": "eb80d5ecad82f8d498d4fa4139320c18",
+    "url": "/aero_club/static/media/Sujeet.eb80d5ec.jpg"
+  },
+  {
+    "revision": "fe9171fd994eeece84a2dbe904549924",
+    "url": "/aero_club/static/media/Ujjwal.fe9171fd.jpeg"
+  },
+  {
+    "revision": "7a24c4fcd5b6e91636caa121cb1ede21",
+    "url": "/aero_club/static/media/Vaibhav.7a24c4fc.jpg"
+  },
+  {
+    "revision": "84ea7c5c9d2fa40c070ccb901046117d",
+    "url": "/aero_club/static/media/Ubuntu-Regular.84ea7c5c.ttf"
+  },
+  {
+    "revision": "c3f02912c424b6342cd80999b97b2531",
+    "url": "/aero_club/index.html"
+  }
+];
